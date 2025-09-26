e21e9b27bc2fb396329a9162594a15974c1bac1d
EthanRGraham <ethan.graham@bsu.edu>
2025-09-26 17:43:49 +0000

    worked on the sstream before class on Friday

--- labfileIO.cpp
+++ labfileIO.cpp
@@ -5,10 +5,27 @@
 int main (){
 	std::string currentLine;
 	std::ifstream inFile;
-"	
+	std::stringstream ss;
+	std::string text;
+	int number;
+
 	inFile.open("data.csv");
 		while (getline(inFile, currentLine)){
-			std:: cout << currentLine << std::endl;
+			ss >> currentLine;
+			std::cout << ss.str();
+			std::cout << currentLine << " testing" << std::endl;
+
+			ss >> number;
+			std::cout << number << std::endl;
+			
+			ss >> number;
+			std::cout << number << std::endl;
+
+			ss >> text;
+			std::cout << text << std::endl;
+
+			
+			//std:: cout << currentLine << std::endl;
 		}// end while
 	inFile.close();
 }//end main
